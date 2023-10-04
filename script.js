023e95b8d16e1c5bb9a69983fae847f76604db7e
jeonhyerin <jeonhyelin19951118@gmail.com>
2023-10-04 13:08:39 +0900

    #4.7 Super Recap - local storage 오리지날 코드

--- script.js
+++ script.js
@@ -7,24 +7,23 @@ const USERNAME_KEY = 'username';
 
 function onLoginSubmit(event) {
 	event.preventDefault();
+	const username = loginInput.value;
 	loginForm.classList.add(HIDDEN_CLASSNAME);
-	localStorage.setItem(USERNAME_KEY, loginInput.value);
-	paintGreetings();
+	localStorage.setItem(USERNAME_KEY, username);
+	paintGreetings(username);
 }
 
-function paintGreetings() {
-	const username = localStorage.getItem(USERNAME_KEY);
+function paintGreetings(username) {
 	greeting.innerText = `Hello ${username}`; 
 	greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
-
 if(savedUsername === null) {
 	//show the form
 	loginForm.classList.remove(HIDDEN_CLASSNAME);
 	loginForm.addEventListener('submit', onLoginSubmit);
 } else {
 	//show the greetings
-	paintGreetings();
+	paintGreetings(savedUsername);
 }
