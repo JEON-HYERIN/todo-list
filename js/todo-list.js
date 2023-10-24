2fdf9a165e0076fc38b7b45dbdff40895bc3f21e
전혜린 <jeonhyerin@hivelab.co.kr>
2023-10-24 17:56:40 +0900

    fix : todo-list.js > 코드 리팩토링

--- js/todo-list.js
+++ js/todo-list.js
@@ -4,6 +4,10 @@ const todoList = document.querySelector('.todo-list');
 
 let todos = []; // 추후에 값을 재할당 할 것이므로 let 사용
 
+function saveTodo() {
+    localStorage.setItem('todos', JSON.stringify(todos));
+}
+
 function createTodo(event) {
     event.preventDefault(); // 기본동작 방지
     const todoText = todo.value;
@@ -11,34 +15,30 @@ function createTodo(event) {
     const todosObj = {
         id: Date.now(),
         text: todoText,
-        checked: false
+        completed: false
     };
     todos.push(todosObj); // 입력받은 값을 todos 배열에 추가
-    localStorage.setItem('todos', JSON.stringify(todos)); // 로컬 스토리지에 저장
+    saveTodo(); // 로컬 스토리지에 저장
     paintTodo(todosObj); // 입력한 투두 리스트를 화면에 보여주는 함수 실행
 }
 
-function paintTodo(todo, index) {
+function paintTodo(todo) {
     const li = document.createElement('li');
     const label = document.createElement('label');
-    const input = document.createElement('input');
+    const checkbox = document.createElement('input');
     const span = document.createElement('span');
     const button = document.createElement('button');
 
     todoList.appendChild(li); // todo-list에 자식 요소로 li 추가(맨 뒤에 추가됨)
     li.id = todo.id;
     li.appendChild(label); // li에 자식 요소로 label 추가
-    label.appendChild(input); // label에 자식 요소로 label 추가
-    input.setAttribute('type', 'checkbox');
-    input.addEventListener('change', function() {
-        todo.checked = !todo.checked;
-        localStorage.setItem('todos', JSON.stringify(todos));
+    label.appendChild(checkbox); // label에 자식 요소로 label 추가
+    checkbox.setAttribute('type', 'checkbox');
+    checkbox.addEventListener('change', function() {
+        todo.completed = !todo.completed;
+        saveTodo();
     });
-    if(todo.checked === true) {
-        input.setAttribute('checked', 'checked');
-    } else {
-        input.removeAttribute('checked');
-    }
+    todo.completed ? checkbox.setAttribute('checked', 'checked') : checkbox.removeAttribute('checked'); // 값이 true라면 체크박스에 checked 속성 추가하고 false면 속성 제거
     label.appendChild(span); // label에 자식 요소로 span 추가
     span.innerText = todo.text;
     li.appendChild(button); // li에 자식 요소로 button 추가
@@ -51,14 +51,14 @@ function removeTodo(event) {
     li.remove(); // 클릭한 li 삭제
     const newTodo = todos.filter((todo) => todo.id !== parseInt(li.id));
     todos = newTodo;
-    localStorage.setItem('todos', JSON.stringify(todos));
+    saveTodo();
 }
 
 todoForm.addEventListener('submit', createTodo);
 
 const savedTodos = localStorage.getItem('todos'); // todo 값 저장하는 변수
 
-// 이미 저장된 todos 값이 있다면
+// 이미 저장된 todos 값이 있다면 해당 조건문 실행
 if(savedTodos !== null) {
     const paresdTodos = JSON.parse(savedTodos); // 문자열로 저장된 데이터를 기존의 원본 데이터로 변환
     todos = paresdTodos; // 기존에 저장되어 있던 값을 todos 배열에 추가
