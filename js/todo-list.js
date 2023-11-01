240ae0220b2bcccac110a61185986ab7e04dcded
전혜린 <jeonhyerin@hivelab.co.kr>
2023-11-01 14:24:59 +0900

    refact : 변수명 변경 등 코드 리팩토링

--- js/todo-list.js
+++ js/todo-list.js
@@ -3,6 +3,12 @@ const todo = (function() {
 	const savedTodosArr = localStorage.getItem('todos'); // todosArr 값 저장하는 변수
 	const els = {};
 	
+	const init = function() {
+		setElements();
+		bindEvents();
+		todoHandler.checkSavedTodosArr();
+	}
+	
 	const setElements = function() {
 		els.section = document.querySelector('.todo');
 		if(!!els.section) { // 느낌표 두개(!!)는 다른 타입의 데이터를 boolean 타입으로 명시적으로 형 변환 하기 위해 사용
@@ -10,7 +16,7 @@ const todo = (function() {
 			els.todoInput = els.section.querySelector('.todo-input');
 			els.todoList = els.section.querySelector('.todo-list');
 			els.todoBtn = els.section.querySelector('.todo-btn');
-			els.todoLength = els.section.querySelector('.todo-total');
+			els.todoTotal = els.section.querySelector('.todo-total');
 			els.todoClearBtn = els.section.querySelector('.clear-btn');
 		}
 	}; 
@@ -29,8 +35,8 @@ const todo = (function() {
 		save: function() {
 			localStorage.setItem('todos', JSON.stringify(todosArr));
 		}, 
-		checkLength: function() {
-			els.todoLength.innerText = JSON.parse(localStorage.getItem('todos')).length;
+		countTotal: function() {
+			els.todoTotal.innerText = JSON.parse(localStorage.getItem('todos')).length;
 		},
 		create: function(event) {
 			event.preventDefault(); // 기본동작 방지
@@ -45,21 +51,21 @@ const todo = (function() {
 	
 			todosArr.push(todosObj); // 입력받은 값을 todosArr 배열에 추가
 			todoHandler.save(); // 로컬 스토리지에 저장
-			todoHandler.checkLength();
+			todoHandler.countTotal();
 			todoHandler.paint(todosObj); // 입력한 투두 리스트를 화면에 보여주는 함수 실행
 		},
 		paint: function(todo) {
-			if ("content" in document.createElement("template")) { // template 태그가 있다면 조건문 실행
-				const template = document.querySelector('#todo-item-template');
-				const cloneTemplate = document.importNode(template.content, true); // importNode를 통해서 template 안쪽 내용 복사(자식노드를 전부 포함해서)
-				const todoItem = cloneTemplate.querySelector('.todo-item'); 
-				const todoCheckBox = cloneTemplate.querySelector('.todo-check');
-				const todoItemText = cloneTemplate.querySelector('.todo-item span');
+			if ("content" in document.createElement("template")) {
+				const todoItemTemplate = document.querySelector('#todo-item-template');
+				const todoItemClone = document.importNode(todoItemTemplate.content, true); // importNode를 통해서 template 안쪽 내용 복사(자식노드를 전부 포함해서)
+				const todoItem = todoItemClone.querySelector('.todo-item'); 
+				const todoCheckBox = todoItemClone.querySelector('.todo-check');
+				const todoItemText = todoItemClone.querySelector('.todo-item span');
 	
-				els.todoList.appendChild(todoItem);
 				todoItem.setAttribute('data-id', todo.id);
 				todoItemText.innerText = todo.text;
-				todoHandler.checkLength();
+				els.todoList.appendChild(todoItemClone);
+				todoHandler.countTotal();
 	
 				// 새로고침해도 체크박스 데이터 유지하기 위한 조건문
 				if(todo.completed) {
@@ -81,7 +87,7 @@ const todo = (function() {
 			targetItem.remove(); // 클릭한 todoList 삭제
 			todosArr = newTodosArr;
 			todoHandler.save(); // 로컬 스토리지에 저장
-			todoHandler.checkLength();
+			todoHandler.countTotal();
 		},
 		clearAll: function() {
 			// 자식요소가 있다면 해당 조건문 실행(아무 때나 다 지우면 안되기 때문에 조건 걸기)
@@ -91,25 +97,19 @@ const todo = (function() {
 			localStorage.clear(); // 로컬스토리지의 모든 데이터 삭제
 			todosArr = []; // 배열 빈 값 할당
 			todoHandler.save();
-			todoHandler.checkLength();
+			todoHandler.countTotal();
+		},
+		checkSavedTodosArr: function() {
+			// 로컬 스토리지에 저장되어 있는 값이 있다면 해당 조건문 실행
+			if (savedTodosArr !== null) {
+				const paresdTodosArr = JSON.parse(savedTodosArr); // 문자열로 저장된 데이터를 기존의 원본 데이터로 변환
+				todosArr = paresdTodosArr; // 기존에 저장되어 있던 값을 todosArr 배열에 추가
+				paresdTodosArr.forEach(todoHandler.paint); // 배열 반복문 실행해서 화면에 출력
+				todoHandler.countTotal();
+			}
 		}
 	};
 	
-	const checkSavedTodosArr = function() {
-		// 로컬 스토리지에 저장되어 있는 값이 있다면 해당 조건문 실행
-		if (savedTodosArr !== null) {
-			const paresdTodosArr = JSON.parse(savedTodosArr); // 문자열로 저장된 데이터를 기존의 원본 데이터로 변환
-			todosArr = paresdTodosArr; // 기존에 저장되어 있던 값을 todosArr 배열에 추가
-			paresdTodosArr.forEach(todoHandler.paint); // 배열 반복문 실행해서 화면에 출력
-			todoHandler.checkLength();
-		}
-	}
-	
-	const init = function() {
-		setElements();
-		bindEvents();
-		checkSavedTodosArr();
-	}
 	return {
 		init: init
 	}
